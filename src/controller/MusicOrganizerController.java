77df9cc06616f9b9201c23542afc1aa6c0190561
Jespris <jesper.johannes.andersson@gmail.com>
2022-04-15 13:13:06 +0300

    Implemented newAlbum to controller

--- src/controller/MusicOrganizerController.java
+++ src/controller/MusicOrganizerController.java
@@ -6,11 +6,14 @@ import java.util.Set;
 
 import model.Album.Album;
 import model.Album.RootAlbum;
+import model.Album.SubAlbum;
 import model.SoundClip;
 import model.SoundClipBlockingQueue;
 import model.SoundClipLoader;
 import model.SoundClipPlayer;
 import view.MusicOrganizerWindow;
+
+import javax.swing.*;
 
 public class MusicOrganizerController {
 
@@ -54,8 +57,13 @@ public class MusicOrganizerController {
 	public void addNewAlbum(){ //TODO Update parameters if needed -
 		//  - e.g. you might want to give the currently selected album as parameter
 		// TODO: Add your code here
-
-		
+		// the currently selected album as parent,
+		// if none selected => parent album is null => root album is parent (defined in SubAlbum constructor)
+		// albumName comes from ? => Set album name to default "New Album"?
+		Album parent = view.getSelectedAlbum();  // get parent album
+		String albumName = JOptionPane.showInputDialog("Album name: ");
+		Album newAlbum = new SubAlbum(albumName, parent);
+		view.onAlbumAdded(newAlbum);
 	}
 	
 	/**
@@ -63,7 +71,16 @@ public class MusicOrganizerController {
 	 */
 	public void deleteAlbum(){ //TODO Update parameters if needed
 		// TODO: Add your code here
-		
+		// delete currently selected album (and all it's subAlbums?)
+		Album album = view.getSelectedAlbum();
+		if (album == RootAlbum.get()){
+
+		} else {
+			for (SubAlbum subAlbum : album.getSubAlbums()) {
+				album.remove(subAlbum);
+			}
+			album.getParentAlbum().remove(album);
+		}
 	}
 	
 	/**
@@ -71,7 +88,8 @@ public class MusicOrganizerController {
 	 */
 	public void addSoundClips(){ //TODO Update parameters if needed
 		// TODO: Add your code here
-		
+		// add sound clip to currently selected album (which automatically adds the clip to all parents)
+		// if no album is selected => add to root?
 	}
 	
 	/**
@@ -79,7 +97,7 @@ public class MusicOrganizerController {
 	 */
 	public void removeSoundClips(){ //TODO Update parameters if needed
 		// TODO: Add your code here
-		
+		// remove from currently selected album currently selected soundclip(s)
 	}
 	
 	/**
